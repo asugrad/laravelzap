bf222e48efe73954919d9b76bb4694e1b4386145
Michael O'Neil <moneil@infinitydirect.com>
2017-02-07 14:01:29 -0500

    Working on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var shell = require("gulp-shell");
 var browserSync = require('browser-sync');
 var livereload = require('gulp-livereload');
+var production = elixir.config.production;
 
 gulp.task('zapBuild', function() {
   return gulp.src("").pipe( shell( [
@@ -12,32 +13,48 @@ gulp.task('zapBuild', function() {
 });
 
 elixir((mix) => {
+
+  if (production) {
+
     mix.sass('app.scss','storage/app/public/css')
-        .copy( 'resources/assets/img/', 'storage/app/public/img/' )
-        .webpack('app.js','storage/app/public/js')
-        .task('zapBuild')
-        .browserSync({
-          proxy: 'laravelzapgithub.dev',
-          port: 8080,
-          notify: false,
-          reloadOnRestart: true,
-          reloadDelay: 1000,
-          fn: function (event, file) {
-            mix.task('zapBuild')
-          },
-          files: [
-                'app/**/*',
-                'public/**/*',
-                'resources/**/*'
-            ],
-        })
+    .copy( 'resources/assets/img/', 'storage/app/public/img/' )
+    .webpack('app.js','storage/app/public/js');
+
+  } else {
+
+    mix.sass('app.scss','storage/app/public/css')
+      .copy( 'resources/assets/img/', 'storage/app/public/img/' )
+      .webpack('app.js','storage/app/public/js')
+      .task('zapBuild')
+      .browserSync({
+        proxy: 'laravelzapgithub.dev',
+        port: 8080,
+        notify: false,
+        reloadOnRestart: true,
+        reloadDelay: 1000,
+        fn: function (event, file) {
+          gulp.src("").pipe( shell( [
+            "php artisan zap:build"
+          ]))
+        },
+        files: [
+          'app/**/*',
+          'public/**/*',
+          'resources/**/*'
+        ],
+      });
+  }
+
 });
 
-gulp.watch("resources/**/*", ['browserSync']).on('change',
-  function(mix) {
-    gulp.src("").pipe( shell( [
-        "php artisan zap:build"
-    ]));
-    browserSync.reload;
-  }
-);
+if (!production) {
+  gulp.watch("resources/**/*", ['browserSync']).on('change',
+
+    function(mix) {
+      gulp.src("").pipe( shell( [
+          "php artisan zap:build"
+      ]));
+      browserSync.reload;
+    }
+  );
+}
