962bf93bec2a54ff35f6c05928c4f233cd0ee93a
Michael O'Neil <moneil@infinitydirect.com>
2017-02-08 17:03:37 -0500

    Updated blog

--- resources/views/layout/partials/js.blade.php
+++ resources/views/layout/partials/js.blade.php
@@ -11,7 +11,10 @@
 
       blog_posts = '';
       $.each(posts, function(index, post) {
+        var date = new Date(post.date);
+
         blog_posts += '<h3 class="ctitle">'+post.title['rendered']+'</h3>';
+        blog_posts += '<h4>'+(date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear()+'</h4>';
         blog_posts += post.content['rendered'];
         blog_posts += '<div class="hline"></div>';
         blog_posts += '<div class="spacing"></div>';
